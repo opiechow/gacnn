545f44df91628c88d2bdafcea30c17809fc04a24
Oskar Piechowski <ospiechowski@gmail.com>
2018-07-31 12:04:31 +0200

    Autostart if fail

--- worker/starter.sh
+++ worker/starter.sh
@@ -2,5 +2,9 @@ rm result
 pkill python
 python3 listener_results.py > /dev/null &
 disown
-python3 listener_jobs.py > /dev/null &
-disown
+while :
+do
+    python3 listener_jobs.py > /dev/null
+    echo "Job listener failed, restarting in 10 seconds"
+    sleep 10
+done
